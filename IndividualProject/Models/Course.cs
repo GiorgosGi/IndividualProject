96e8c08015c0ec2afa0af14510419ac36fc05352
Giorgos Gial <g.gialouris@outlook.com.gr>
2020-11-05 18:13:33 +0200

    Add project files.

---
+++ IndividualProject/Models/Course.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndividualProject.Models
+{
+    class Course
+    {
+        private string   _title;  
+        private string   _stream;
+        private string   _type;
+        private DateTime _start_date;
+        private DateTime _end_date;
+
+        public string Title  
+        {
+            get { return (this._title); }
+            set { this._title = value; }
+        }
+
+        public string Stream
+        {
+            get { return (this._stream); }
+            set { this._stream = value; }
+        }
+
+        public string Type
+        {
+            get { return (this._type); }
+            set { this._type = value; }
+        }
+
+        public DateTime Start_Date
+        {
+            get { return (this._start_date); }
+            set { this._start_date = value; }
+        }
+
+        public DateTime End_Date
+        {
+            get { return (this._end_date); }
+            set { this._end_date = value; }
+        }
+
+        public override string ToString()      
+        {
+            return ($"Title: {_title}\tStream: {_stream}\tType: {_type}\tStart Date: {_start_date}\tEnd Date: {_end_date} "); 
+        }
+    }
+}
---
+++ IndividualProject/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IndividualProject.Models;
+using IndividualProject.BusinessLogic;
+
+namespace IndividualProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandPromptUtils cpUtils = new CommandPromptUtils();
+
+            Console.WriteLine("How many traines has the school?");
+            int tr = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= tr; i++)
+            {
+                List<Trainer> trainers = new List<Trainer>();
+                trainers.Add(cpUtils.GetTrainerDetails());
+                cpUtils.PrintTrainersList(trainers);
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("How many courses the school offers?");
+            int crs = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= crs; i++)
+            {
+                List<Course> courses = new List<Course>();
+                courses.Add(cpUtils.GetCourseDetails());
+                cpUtils.PrintCoursesList(courses);
+            }
+
+            Console.WriteLine("How many students are in the school?");
+            int std = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= std; i++)
+            {
+                List<Student> students = new List<Student>();
+                students.Add(cpUtils.GetStudentDetails());
+                cpUtils.PrintStudentsList(students);
+            }
+
+
+            Console.WriteLine("How many are the current assignments?");
+            int assgn = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= assgn; i++)
+            {
+                List<Assignment> assignments = new List<Assignment>();
+                assignments.Add(cpUtils.GetAssignmentDetails());
+                cpUtils.PrintAssignmentsList(assignments);
+            }
+
+        }
+
+       
+        // Create appropriate methods in order to ask 
+        // from the user data for the four main classes
+    }
+}
